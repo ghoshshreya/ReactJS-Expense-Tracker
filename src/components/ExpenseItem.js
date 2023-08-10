9f610f18331445438fa667d492ad18a2259f0236
ghoshshreya <shrghosh08@gmail.com>
2023-08-10 23:01:13 +0530

    Outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import './ExpenseItem.css';
 export default function ExpenseItem(props) {
+  const expenseDate = new Date(2023, 2, 29); // March 29, 2023
+  const expenseTitle = 'Car Insurance';
+  const expensePrice = 219;
   return (
     <div className="expense-item">
-      <div>March 29, 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$219</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
       </div>
     </div>
   );
