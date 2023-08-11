0b28608bcd05f9ae90d00e1f652a1ea83fae0d76
ghoshshreya <shrghosh08@gmail.com>
2023-08-11 12:30:43 +0530

    Alertnative way of passing props

--- src/App.js
+++ src/App.js
@@ -26,23 +26,11 @@ export default function App() {
   ];
   return (
     <div>
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
+      <ExpenseItem expense={expenses[0]}></ExpenseItem>
 
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
+      <ExpenseItem expense={expenses[1]}></ExpenseItem>
 
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
+      <ExpenseItem expense={expenses[2]}></ExpenseItem>
     </div>
   );
 }
