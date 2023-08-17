3d74393d71ade961e1f871c9297afd0e02c3cc47
ghoshshreya <shrghosh08@gmail.com>
2023-08-17 15:27:02 +0530

    2 files modified, 2 added

---
+++ src/components/Card.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import './Card.css';
+export default function Card(props) {
+  return <div className="card">{props.children}</div>;
+}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import './ExpenseItem.css';
+import Card from './Card';
 export default function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <div>{props.expense.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.expense.title}</h2>
-        <div className="expense-item__price">${props.expense.price}</div>
+        <div className="expense-item__price">${props.expense.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
